cd3920191f69ebe0a986402b87cc9e88707338b4
Francesiyamu <francessiyamu01@gmail.com>
2024-04-15 16:59:45 +0200

    js updated

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+
 const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
+
+const loginLink = document.querySelector('.login-link')
+
 const registerLink = document.querySelector('.register-link');
+
+/*const btnPopUp = document.querySelector('.headerbtnLogin-popup');*/
+
 
 
 registerLink.addEventListener('click', ()=> {
-  wrapper.classList.add('active');
-});
+wrapper.classList.add('active');
+})
 
 
 loginLink.addEventListener('click', ()=> {
   wrapper.classList.remove('active');
-});
+})
+
+/*
+btnPopUp.addEventListener('click', ()=> {
+  wrapper.classList.remove('active-popup');
+})*/
