4af9980169220f455aa96ee40250f1910c3770c5
Annemie Vandecandelaere <annemie.vandecandelaere@student.ehb.be>
2024-05-03 19:08:37 +0200

    nieuwe gebruiker aanmaken met gehashed and salted psw

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const routes = require('./routes');
+const session = require('express-session');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(session({
+    secret  : 'secret', // session secret
+    resave: false, // save session on every request
+    saveUninitialized: false,   // don't create session until something stored
+}));
+
 app.use('/',routes);
+
+
+
 
 
 // Start server
