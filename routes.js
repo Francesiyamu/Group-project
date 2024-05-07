e6b49e01da9bb61a53fcdc2573d8e126966d2378
Annemie Vandecandelaere <annemie.vandecandelaere@student.ehb.be>
2024-05-07 17:06:40 +0200

    routes.js file changes

--- routes.js
+++ routes.js
@@ -2,23 +2,19 @@ console.log('Hello from the routes file!');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 const connection = require('./db_connection');
 const { registreerNieuwProject } = require('./controllers/projectController');
 const { registreerGebruiker } = require('./controllers/registerController');
 
 //---------HOME-------------------------------------------------------------
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'login', 'login.html'));
+    res.sendFile(path.join(__dirname, 'views', 'login','login.html'));
 });
-
 router.get('/login.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'login', 'login.css'), {
-        headers: {
-            'Content-Type': 'text/css'
-        }
-    });
+    res.sendFile(path.join(__dirname, 'views', 'login', 'login.css'));
+    res.setHeader('Content-Type', 'text/css');
 });
-
 router.get('/login.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login', 'login.js'));
 });
@@ -29,7 +25,20 @@ router.get('/nieuw_project', (req, res) => {
 });
 
 router.get('/nieuw_project.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'projecten', 'nieuw_project.css'));
+    const ccsFilesProject =[
+        path.join(__dirname, 'views', 'projecten', 'nieuw_project.css'),
+        path.join(__dirname, 'views', 'CSS', 'projecten.css'), ///???
+        path.join(__dirname, 'views', 'CSS','home.css')// ????
+    ];
+
+    let combinedCss = '';
+    ccsFilesProject.forEach(file => {
+        combinedCss += fs.readFileSync(file, 'utf8');
+    });
+
+    res.setHeader('Content-Type', 'text/css');
+    res.send(combinedCss);
+    
 });
 
 router.get('/nieuw_project.js', (req, res) => {
@@ -42,11 +51,19 @@ router.get('/nieuwe_gebruiker', (req, res) => {
 });
 
 router.get('/nieuwe_gebruiker.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'gebruikers', 'nieuwe_gebruiker.css'), {
-        headers: {
-            'Content-Type': 'text/css'
-        }
+    const ccsFilesGebruiker =[
+        path.join(__dirname, 'views', 'gebruikers', 'nieuwe_gebruiker.css'),
+        path.join(__dirname, 'views', 'CSS', 'leveranciers_gebruikers.css'), // ??????
+        path.join(__dirname, 'views', 'CSS','home.css') // ??????
+    ];
+
+    let combinedCss = '';
+    ccsFilesGebruiker.forEach(file => {
+        combinedCss += fs.readFileSync(file, 'utf8');
     });
+
+    res.setHeader('Content-Type', 'text/css');
+    res.send(combinedCss);
 });
 
 router.get('/nieuwe_gebruiker.js', (req, res) => {
