7d1403db3ce9309e54893800a8cc426a9da4db53
Annemie Vandecandelaere <annemie.vandecandelaere@student.ehb.be>
2024-05-10 20:11:00 +0200

    modification authController

--- views/JS/nieuwe_gebruiker.js
+++ views/JS/nieuwe_gebruiker.js
@@ -1,14 +1,25 @@
-
 console.log('Hello from gebruikersscript !');
-
 
 const serverUrl = 'http://localhost:3000';
 
-document.getElementById('form').addEventListener('submit', function(e) {
+
+
+document.getElementById('form_gebruiker').addEventListener('submit', async function(e) {
     e.preventDefault();
 
+    function getFunctieNr(functie){
+        if(functie === "manager"){
+            return 1;
+        }else if(functie === "medewerker"){
+            return 2;
+        }else if(functie === "boekhouder"){
+            return 3;
+        }
+    }
+    
     const formData = {
-        id : document.getElementById('idnr').value,
+        //id : document.getElementById('idnr').value,
+        //funtienr : getFunctieNr(document.getElementById('functie').value),
         gebruikersnaam: document.getElementById('gebruikersnaam').value,
         wachtwoord: document.getElementById('wachtwoord').value,
         voornaam: document.getElementById('voornaam').value,
@@ -20,11 +31,12 @@ document.getElementById('form').addEventListener('submit', function(e) {
         land : document.getElementById('gebrland').value,
         telefoonnummer: document.getElementById('gebrtelefoon').value,
         emailadres: document.getElementById('emailadres').value,
-      
+        
 
     };
 
     const json = JSON.stringify(formData);
+    
     console.log(formData);
 
     fetch('/submit-form-nieuwe-gebruiker' ,{
