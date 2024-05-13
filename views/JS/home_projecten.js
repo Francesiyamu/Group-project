8a84b33b6c9e3e4fdeb32a65467521029e8c439c
JariSimons <146962826+JariSimons@users.noreply.github.com>
2024-05-13 15:24:18 +0200

    Create home_projecten.js
    JS-bestand voor home_projecten aangemaakt.

---
+++ views/JS/home_projecten.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    console.log('loaded');
+    
+
+    let selected = Array.from(document.getElementsByClassName('contentLijnen'));
+
+    // DISABLE TEXT SELECTION WHEN DOUBLE CLICK --> TO DO
+
+
+    for(let element of selected) {
+        element.style.cursor = 'pointer'; 
+        element.addEventListener('click',function(){
+            window.location.href = "../klant_factuur/details_aanpassen_project.html"; //Zo? Of elk element in lijst als a? https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+        })
+    }
+}
