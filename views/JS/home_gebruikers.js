f0d7cf1825686798bdbc290141005936af6f4c3e
jihadac <jihad.achaoui@hotmail.com>
2024-05-15 22:50:32 +0200

    Click event search icon kleinere schermen

--- views/JS/home_gebruikers.js
+++ views/JS/home_gebruikers.js
@@ -1,37 +1,42 @@
 window.onload = () => {
     console.log('loaded');
-    
-    let selected = document.getElementById('test');
-    selected.style.cursor = 'pointer'; 
 
-    // DISABLE TEXT SELECTION WHEN DOUBLE CLICK --> TO DO
-  
-    selected.addEventListener('click',function(){
-        window.location.href = "../gebruikers/details_aanpassen_gebruiker.html"; //Zo? Of elk element in lijst als a? https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+    let search_icon_btn1 = document.getElementById('search_icon_btn1');
+    let visible = false; //In beginning, search bar is not visible
+    search_icon_btn1.addEventListener('click',function(){
+        // Show search bar
+        let searchbar_small = document.getElementById('searchbar_small');
+        if(!visible) {
+            searchbar_small.style.display = 'initial';
+            visible = true;
+        } else {
+            searchbar_small.style.display = 'none';
+            visible = false;
+        }
     })
+    
 
+    function tableSearch() {
+        let input, filter, table, tr, td, i, j, txtValue;
     
-}
-function tableSearch() {
-    let input, filter, table, tr, td, i, j, txtValue;
+        input = document.getElementById("searchBox");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("table").getElementsByTagName("table")[0];
+        tr = table.getElementsByTagName("tr");
     
-    input = document.getElementById("searchBox");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("table").getElementsByTagName("table")[0];
-    tr = table.getElementsByTagName("tr");
-    
-    // Loop through all table rows, excluding the header row
-    for (i = 1; i < tr.length; i++) {
-        tr[i].style.display = "none"; // Hide the row initially
+        // Loop through all table rows, excluding the header row
+        for (i = 1; i < tr.length; i++) {
+            tr[i].style.display = "none"; // Hide the row initially
         
-        td = tr[i].getElementsByTagName("td");
-        // Loop through all table columns
-        for (j = 0; j < td.length; j++) {
-            if (td[j]) {
-                txtValue = td[j].textContent || td[j].innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = ""; // Show the row if a match is found
-                    break; // Stop checking other columns in this row
+            td = tr[i].getElementsByTagName("td");
+            // Loop through all table columns
+            for (j = 0; j < td.length; j++) {
+                if (td[j]) {
+                    txtValue = td[j].textContent || td[j].innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = ""; // Show the row if a match is found
+                        break; // Stop checking other columns in this row
+                    }
                 }
             }
         }
--- views/JS/home_leveranciers.js
+++ views/JS/home_leveranciers.js
@@ -1,14 +1,29 @@
 window.onload = () => {
     console.log('loaded');
     
-    let selected = document.getElementById('test');
+ /*   let selected = document.getElementById('test');
     selected.style.cursor = 'pointer'; 
 
     // DISABLE TEXT SELECTION WHEN DOUBLE CLICK --> TO DO
   
     selected.addEventListener('click',function(){
         window.location.href = "../leveranciers/details_aanpassen_leverancier.html"; //Zo? Of elk element in lijst als a? https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+    }) */
+
+    let search_icon_btn1 = document.getElementById('search_icon_btn1');
+    let visible = false; //In beginning, search bar is not visible
+    search_icon_btn1.addEventListener('click',function(){
+        // Show search bar
+        let searchbar_small = document.getElementById('searchbar_small');
+        if(!visible) {
+            searchbar_small.style.display = 'initial';
+            visible = true;
+        } else {
+            searchbar_small.style.display = 'none';
+            visible = false;
+        }
     })
+
 
     
 }
