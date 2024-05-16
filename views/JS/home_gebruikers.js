b056574c27eaede530d7912bfd9a2a0697fc1596
jihadac <jihad.achaoui@hotmail.com>
2024-05-17 00:13:50 +0200

    Verwijder click event search btn

--- views/JS/home_gebruikers.js
+++ views/JS/home_gebruikers.js
@@ -1,19 +1,5 @@
 window.onload = () => {
     console.log('loaded');
-
-    let search_icon_btn1 = document.getElementById('search_icon_btn1');
-    let visible = false; //In beginning, search bar is not visible
-    search_icon_btn1.addEventListener('click',function(){
-        // Show search bar
-        let searchbar_small = document.getElementById('searchbar_small');
-        if(!visible) {
-            searchbar_small.style.display = 'initial';
-            visible = true;
-        } else {
-            searchbar_small.style.display = 'none';
-            visible = false;
-        }
-    })
 }
 
 function tableSearch() {
