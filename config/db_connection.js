99858aec9ee016d981f98312524238863af590d8
programmasters <maesschalck.bart@gmail.com>
2024-05-19 15:10:50 +0200

    Update db_connection.js
    Added a connection time out check/disconnect fail safe for the database

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,21 +1,37 @@
-
 const mysql = require('mysql2');
 
 // Create MySQL connection
-const connection = mysql.createConnection({
-    host: '10.0.1.50', //  IP address
+const db_config = {
+    host: '10.0.1.50',
     user: 'Developer',
     password: 'RADEPIREMENT',
     database: 'Datafact'
-});
+};
 
-// Connect to MySQL
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); 
+
+  connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    } else {
+      console.log('connected to db');
+    }                                    
+  });                                   
+                                         
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else {                                     
+      throw err;                                 
     }
-    console.log('Connected to MySQL');
-});
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
