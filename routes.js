e0cbf568e97759aea9a204b98781e6cd23271e62
jihadac <jihad.achaoui@hotmail.com>
2024-05-24 00:46:07 +0200

    Verwerking errors bij toevoegen/aanpassen leverancier

--- routes.js
+++ routes.js
@@ -174,7 +174,15 @@ router.post('/leveranciers/submission_nieuwe_leverancier_form', authenticateToke
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        let errorMsg = [];
+        for(let error of errors.array()) {
+            errorMsg.push(error.msg);
+        }
+    
+        StringifiedErrorMsg = JSON.stringify(errorMsg);
+        StringifiedSubmittedData = encodeURIComponent(JSON.stringify(req.body)); // Necessary to recognize + of telefoonnr, expects string
+        res.redirect(`/leveranciers/nieuwe_leverancier.html?errorsSubmission=${StringifiedErrorMsg}&submittedData=${StringifiedSubmittedData}`);
+        //return res.status(400).json({ errors: errors.array() });
     } else {
         try {
             const {levnr, levnaam, straatnaam, huisnr, postcode, gemeente, land, telefoonnr, email, BTWnr} = req.body;
@@ -188,6 +196,29 @@ router.post('/leveranciers/submission_nieuwe_leverancier_form', authenticateToke
     }
 })
 
+// Details leverancier
+router.get('/leveranciers/details_aanpassen_leverancier.html', (req,res) => {
+    console.log('details leverancier');
+    const id = req.query.nr;
+    connection.query('SELECT levnr, naam, straatnaam, huisnr, gemeente, postcode, land, telefoonnr, emailadres, BTWnr FROM LEVERANCIERS WHERE levnr = ?', [id], (error, results) => {
+        console.log(results[0]);
+
+    // Check if errors from previous submission
+        let errorMsg;
+        if(req.query.errorsSubmission){ 
+            const errorsSubmission = req.query.errorsSubmission;
+            errorMsg = JSON.parse(errorsSubmission);
+        }
+
+    // Render page
+        if(errorMsg) {
+            res.render(path.join(__dirname, 'views', 'leveranciers', 'details_aanpassen_leverancier'), {leverancier: results[0], errors: errorMsg});
+        } else {
+            res.render(path.join(__dirname, 'views', 'leveranciers', 'details_aanpassen_leverancier'), {leverancier: results[0]});
+        }
+    })
+})
+
 // Aanpassen leverancier
 router.post('/leveranciers/submission_update_leverancier_form', authenticateToken, validationRulesLev(), async (req, res) => {
     console.log('aanpassen leverancier');
@@ -196,7 +227,15 @@ router.post('/leveranciers/submission_update_leverancier_form', authenticateToke
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        let errorMsg = [];
+        for(let error of errors.array()) {
+            errorMsg.push(error.msg);
+        }
+        
+        let nr = req.body.levnr;
+        StringifiedErrorMsg = JSON.stringify(errorMsg);
+        res.redirect(`/leveranciers/details_aanpassen_leverancier.html?nr=${nr}&errorsSubmission=${StringifiedErrorMsg}`);
+        //return res.status(400).json({ errors: errors.array() });
     } else {
         try {
             const {levnr, levnaam, straatnaam, huisnr, postcode, gemeente, land, telefoonnr, email, BTWnr} = req.body;
